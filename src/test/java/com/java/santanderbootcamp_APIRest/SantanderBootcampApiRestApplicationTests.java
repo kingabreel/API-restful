461bb4a04f7bc67a690643dcb0a6d4fd0f027cda
kingabreel <gabrielyury441@gmail.com>
2023-09-13 23:08:24 -0300

    Initial commit

---
+++ src/test/java/com/java/santanderbootcamp_APIRest/SantanderBootcampApiRestApplicationTests.java
@@ -0,0 +1,13 @@
+package com.java.santanderbootcamp_APIRest;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SantanderBootcampApiRestApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
